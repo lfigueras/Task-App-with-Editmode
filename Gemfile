1fd4d5ee251af3cd370c03a4cb30256a9b041342
lfigueras <lovelyfigueras@gmail.com>
2020-12-05 22:02:55 +0800

    finished login devise.

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
+gem 'devise', '~> 4.7', '>= 4.7.3'
 
 group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-get 'task/index'
+  
 
-get   '/tasks/:id/edit' => 'tasks#edit', as: 'edit_task'
-patch   '/tasks/:id/edit' => 'tasks#update', as: 'update_task'
-get   '/tasks/:id' => 'tasks#show', as: 'show_task'
-resources :categories do
-    resources :tasks, except: [:edit, :update, :show]
-end
+    devise_for :users
 
-root 'tasks#index'
+    get   '/tasks/:id/edit' => 'tasks#edit', as: 'edit_task'
+    patch   '/tasks/:id/edit' => 'tasks#update', as: 'update_task'
+    get   '/tasks/:id' => 'tasks#show', as: 'show_task'
+    resources :categories do
+        resources :tasks, except: [:edit, :update, :show]
+    end
+  
+    get 'task/index'
+    root 'tasks#index'
 
 end
