bb627bb64cb7fecc2ff38937565ccfbab7e182cf
Franz <Franz@LAPTOP-JC7PJSNR>
2020-12-08 20:19:58 +0800

    fixed all bugs.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,8 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
+    @category.user_id = current_user.id
+
     if @category.save
      redirect_to categories_path
      else
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_123011) do
+ActiveRecord::Schema.define(version: 2020_12_08_114609) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
